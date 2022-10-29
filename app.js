6eb02372472f7a6ca8d4f41c685a0688f14813f3
Alex <110700081+Alexmb356@users.noreply.github.com>
2022-10-29 13:23:44 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 
+let principal= document.getElementById('principal').classList;
+console.log(principal);
+let comprar= document.getElementById('comprar').classList;
 
-function resumen(){
-    
-    var cant =document.getElementById("cantidad").value;
-    console.log(cant)
-    var Est =document.getElementById("categoria").value;
-    console.log(Est)
-    var a= parseInt(cant)
-    console.log(a)
-    if (Est=="Estudiante"){
-        var total= a * (200*0.2)
-        
-    }else if(Est=="Trainee"){
-        var total= a * (200*0.5)
-    }else
-        var total= a * (200*0.85)
-    
-    console.log(total)
-    
-    document.getElementById("pagar").innerHTML= "Total a pagar $" + total;
 
+function compras(){
+    principal.add("comprart")
+    console.log(principal)
+    comprar.remove("comprart")
+    
 }
-function reset(){
-    let formulario = document.getElementById('formulario');
-    formulario.reset();
-    document.getElementById("pagar").innerHTML= "Total a pagar $";
-}
+document.getElementById("buy").addEventListener('click', compras);
+
+document.getElementById("buy1").addEventListener('click', compras);
+
+const ticketPrice = 200
+const discountOption = new Map()
+discountOption.set('estudiante', 0.8)
+discountOption.set('trainee', 0.5)
+discountOption.set('junior', 0.15)
+
+const qtyElement = document.getElementById('cantidad')
+const categoryElement = document.getElementById('categoria')
+const priceElement = document.getElementById('pagar')
+const buyTicketsFormElement = document.getElementById('formulario')
+
+buyTicketsFormElement.addEventListener('submit',
+    (event) => {
+        event.preventDefault()
+        if (!buyTicketsFormElement.checkValidity()) {
+            event.stopPropagation();
+            buyTicketsFormElement.classList.add('was-validated')
+        } else if (discountOption.has(categoryElement.value)) {
+            const priceWithDiscount = ticketPrice * (1 - discountOption.get(categoryElement.value)).toPrecision(3)
+            priceElement.value = "Total a Pagar: $ " + (qtyElement.value * priceWithDiscount)
+            console.log(priceElement)
+        }
+    }
+)
+
+buyTicketsFormElement.addEventListener('reset',
+    () => {
+        buyTicketsFormElement.classList.remove('was-validated')
+    }
+)
+
 
 
 
